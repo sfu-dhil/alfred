c72a3992be18b8fb33ed17cdf54f5b1053f81700
Michael Joyce <ubermichael@gmail.com>
2019-03-14 21:38:22 -0700

    make the list of stop word files generated at compile time.

--- src/main/java/ca/nines/alfred/cmd/StopWords.java
+++ src/main/java/ca/nines/alfred/cmd/StopWords.java
@@ -24,48 +24,20 @@
 package ca.nines.alfred.cmd;
 
 import org.apache.commons.io.IOUtils;
-import sun.misc.Launcher;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 @ca.nines.alfred.cmd.CommandInfo(name = "sw", description = "Show info about stop word lists")
 public class StopWords extends Command {
 
     public static final String PATH = "stopwords";
 
-    private List<String> listStopWordFiles() throws IOException, URISyntaxException {
-        List<String> filenames = new ArrayList<>();
-        File file = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
-        if (file.isFile()) {  // Run with JAR file
-            JarFile jar = new JarFile(file);
-            Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
-            while (entries.hasMoreElements()) {
-                String name = entries.nextElement().getName();
-                if (name.startsWith(PATH + "/")) { //filter according to the path
-                    filenames.add(name.substring(PATH.length() + 1));
-                }
-            }
-            jar.close();
-        } else {
-            URL url = Launcher.class.getResource("/" + PATH);
-            if (url != null) {
-                final File apps = new File(url.toURI());
-                for (File app : apps.listFiles()) {
-                    System.out.println(app);
-                }
-            }
-        }
-        return filenames;
+    private List<String> listStopWordFiles() throws IOException {
+        InputStream in = this.getClass().getResourceAsStream("/stopwords/index");
+        return IOUtils.readLines(in, StandardCharsets.UTF_8);
     }
 
     @Override
